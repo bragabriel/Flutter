11c8d1d3946fdd2af3e8046e14461ca14562c57e
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-10-22 22:02:30 -0300

    Initial commit

---
+++ cursoflutter/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(Container(
+    child: Center(
+      child: Text(
+      'Brincando com Flutter :)',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.white, fontSize: 20.0),)
+    ),
+  ));
+}
