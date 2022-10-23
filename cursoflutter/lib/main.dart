b3b197ad5a9fa344b4412f529ae0859ee971ead0
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-10-22 22:17:20 -0300

    StatelessWidget

--- cursoflutter/lib/main.dart
+++ cursoflutter/lib/main.dart
@@ -1,12 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 main(){
-  runApp(Container(
-    child: Center(
-      child: Text(
-      'Brincando com Flutter :)',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(color: Colors.white, fontSize: 20.0),)
-    ),
-  ));
+  runApp(AppWidget(title: 'Brincando com Flutter'));
+}
+
+class AppWidget extends StatelessWidget {
+ 
+  final String title;
+  
+  const AppWidget({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+        title,
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.white, fontSize: 20.0),)
+      ),
+    );
+  }
+
 }
