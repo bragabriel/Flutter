7ffa1073e4b9e618c98827e0979dc34ca4949542
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-11-25 20:55:11 -0300

    StatefulWidget

--- cursoflutter/lib/main.dart
+++ cursoflutter/lib/main.dart
@@ -15,18 +15,43 @@ class AppWidget extends StatelessWidget {
   }) : super(key: key);
 
  
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.red
       ),
-      home: Container(
-        child: Center(child: Text('Flutterando'))
-      ),
+      home: HomePage(),
+      
     );
-    
+  }
+}
+
+  //StatefulWidget
+  class HomePage extends StatefulWidget{
+    @override
+    State<StatefulWidget> createState(){
+      return HomePageState();
+    }
+  }
+
+//Criando uma classe para ser retornada e funcionar como um estado
+class HomePageState extends State<HomePage>{
+
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+        child: Center(child: GestureDetector(child: Text('Contador: $counter'), onTap: (){
+          setState(() {
+            counter++; //necessario dizer que o estado vai ser modificado e precisa ser reconstuido
+          });
+          
+        }))
+      );
     
   }
+
 }
