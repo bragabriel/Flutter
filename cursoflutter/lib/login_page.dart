756cb2c2f04f5b986ee3d244362eaa4144b06674
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-01-04 22:38:31 -0300

    Adicionando Imagem da Internet

--- cursoflutter/lib/login_page.dart
+++ cursoflutter/lib/login_page.dart
@@ -27,6 +27,13 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  child: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Logo_TV_2015.png/250px-Logo_TV_2015.png')),
+                  Container(
+                    height: 20,
+                  ),
                 TextField( 
                   onChanged: (text){
                     email = text;
