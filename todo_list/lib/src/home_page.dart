6a40e6a9ce58ef8eedab99cf23043d895b6fda88
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-01-24 22:30:01 -0300

    Gerenciamento de estados

--- todo_list/lib/src/home_page.dart
+++ todo_list/lib/src/home_page.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'controllers/home_controller.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('List Todo\'s'),
-      ),
-      body: ListView.builder(
+  _success(){
+    return ListView.builder(
         itemCount: 10,
         itemBuilder: (context, index) { 
               return ListTile(
@@ -18,7 +15,47 @@ class HomePage extends StatelessWidget {
                 );
             },
        
-      )
+      );
+  }
+
+  _error(){
+    return ElevatedButton(
+        onPressed: () {}, 
+        child: Text('Tentar novamente'),
+      );
+  }
+
+  _loading(){
+    return Center(
+      child: CircularProgressIndicator()
+    );
+  }
+
+  _start(){
+    return Container();
+  }
+
+  stateManagement(HomeState state){
+    switch (state) {
+      case HomeState.start:
+        return _start();
+      case HomeState.loading:
+        return _loading();
+      case HomeState.error:
+        return _error();
+      case HomeState.success:
+        return _success();
+      default: _start();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List Todo\'s'),
+      ),
+      body:  stateManagement(HomeState.start)
     );
   }
 }
